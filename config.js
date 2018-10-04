513787e027232363b57a8afbc44663a201849b45
Marco Alexi Sta Ana <alexistaana@yahoo.com.ph>
2018-10-04 14:47:52 -0700

    fixed config file

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ exports.DATABASE_URL =
     global.DATABASE_URL ||
     'mongodb://admin:password1@ds121183.mlab.com:21183/studiouso-user';
 
-exports.TEST_DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:password1@ds121593.mlab.com:21593/studiouso-user-tests';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://admin:password1@ds121593.mlab.com:21593/studiouso-user-tests';
 
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
